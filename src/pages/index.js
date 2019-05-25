8fc6ee4904a4f4a675fe69c5279e264399ea7586
chsrinu <chsreenivas92@gmail.com>
2019-05-25 11:16:10 +0530

    Intro page ready

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
 import React from "react"
 
-export default () => <div>Hello Sreeni!</div>
+export default () => 
+<div className="introContainer">
+        <div style={{ "display": "flex", "flexDirection": "column" }}>
+            <div className="nameContainer">
+                I<span>'</span>M <br /> SRINIVAS <br /> CHEERLA<span>.</span>
+            </div>
+            <div className="workTitle"><span>Full-Stack Developer </span></div>
+        </div>
+        <div className="dpContainer">
+            <div className="card">
+                <div className="front">
+                    <img className="imageProps" src="./dp.jpg" alt="dp" />
+                </div>
+                <div className="back">
+                    Aims and Aspirations
+                </div>
+            </div>
+        </div>
+
+ </div>
+
