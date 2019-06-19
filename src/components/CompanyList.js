a61b48bcccaa2dd02462a04c2af25ce5c63be2c1
chsrinu <chsreenivas92@gmail.com>
2019-06-19 23:36:56 +0530

    Fully functional profile

--- src/components/CompanyList.js
+++ src/components/CompanyList.js
@@ -1,22 +1,22 @@
 import React from "react"
 import data from "../../companyWiseResponsibilities.json"
-import Modal from "./Mymodal"
+//import Modal from "./Mymodal"
 import styles from "./companyList.module.css"
 
-function handleClose(id) {
-    document.getElementById(id).style.display="none"
-}
+// function handleClose(id) {
+//     document.getElementById(id).style.display="none"
+// }
 
-function showDescriptions_onClick(id) {
-    document.getElementById(id).style.display="block"
-}
+// function showDescriptions_onClick(id) {
+//     document.getElementById(id).style.display="block"
+// }
 
 export default ({id}) => (
 
 <div id={id} className={styles.container}>
     {data.map((elem)=>(
         <div>
-        <img className="l_img" src={require("../../static/" + elem.src)} alt={elem.src} />
+        <img className="l_img" title={elem.title} src={require("../../static/" + elem.src)} alt={elem.src} />
         <p className="xs">{elem.role}</p>
         </div>
     ))}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,10 @@ import linkedIdIcon from "../../static/linkedIn.png"
 import stackoverflowIcon from "../../static/stackoverflow.png"
 import ScrollDownAnchor from "../components/ScrollDownAnchor"
 import upArrowIcon from "../../static/uparrow.png"
-
+import dollar from "../../static/dollar.png"
+import resume from "../../static/Srinivas Cheerla.pdf"
 import styles from "./index.module.css"
+import { Helmet } from "react-helmet"
 
 function scrollToExperiencePage(){
     document.querySelector('#anchorOneTarget').scrollIntoView({ 
@@ -22,6 +24,14 @@ function scrollToCommunicationChannelPage() {
 
 export default () => 
 <div>
+<Helmet>
+    <meta charSet="utf-8" />
+    <title>Srinivas Cheerla</title>
+    <meta name="description" content="Personal profile of Srinivas Cheerla" />
+    <meta name="keywords" content="Job, looking for Job change, java developer,
+    full stack developer" />
+    <link rel="shortcut icon" type="image/jpg" href={dollar} sizes="16x16" />
+</Helmet>
 <div className="introContainer">
         <div style={{ "display": "flex", "flexDirection": "column" }}>
             <div className="nameContainer">
@@ -38,14 +48,16 @@ export default () =>
                     <div className="s">Aims and Aspirations</div>
                     <p className="xs">Have build myself consistently 
                     with good programming tech stack in 4+ years of 
-                    experience. Willing to leverage my expertise and 
+                    experience. Love to leverage my expertise and 
                     improvise them on a day to day basis. Willing to
                     be a part of team who wants to build applications
                     using cutting edge technologies.
                     </p>
                 </div>
             </div>
-            <img className={`xs_img ${styles.resumeLink}`} src={upArrowIcon} alt="resumeLink" />
+            <a href={resume} download>
+            <img className={`xs_img ${styles.resumeLink}`} src={upArrowIcon} title="download resume" alt="resumeLink" />
+            </a>
             <div className="xxs" style={{"color":"white"}}><i>Click me</i></div>
             
         </div>
@@ -54,10 +66,9 @@ export default () =>
  <CompanywiseExperice id="anchorOneTarget"/>
  <ScrollDownAnchor onClick={scrollToCommunicationChannelPage} caption="Communication channels"/>
  <div id="anchorTwoTarget" className="communication_channel_styles">
-     <a href="https://github.com/chsrinu" target="_blank"><img className="l_img" src={gitIcon} alt="gitUrl" /></a>
-     <a href="https://stackoverflow.com/users/5142300/srinivas" target="_blank"><img className="l_img" src={stackoverflowIcon} alt="gitUrl" /></a>
-     
-     <a href="https://www.linkedin.com/in/srinivas-cheerla-22370a91/" target="_blank"><img className="l_img" src={linkedIdIcon} alt="linkedIn_profile" /></a> 
+     <a rel="noopener noreferrer" href="https://github.com/chsrinu" target="_blank"><img className="l_img" src={gitIcon} alt="gitUrl" /></a>
+     <a rel="noopener noreferrer" href="https://stackoverflow.com/users/5142300/srinivas" target="_blank"><img className="l_img" src={stackoverflowIcon} alt="gitUrl" /></a>
+     <a rel="noopener noreferrer" href="https://www.linkedin.com/in/srinivas-cheerla-22370a91/" target="_blank"><img className="l_img" src={linkedIdIcon} alt="linkedIn_profile" /></a> 
  </div>
 
  </div>
